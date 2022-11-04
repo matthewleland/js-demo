30e341b8bcc0114325d5667d3dc85f37a59c9b31
Matthew Leland <mleland18@apu.edu>
2022-11-04 13:47:59 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+////////////////////////////////////////////////
+// INITIALIZING VARIABLES: let, var, and const
+////////////////////////////////////////////////
+// {
+//   let x = 'Matthew'
+//   console.log('My name is: ' + x)
+//   x = 'Seth'
+//   console.log('My new name is: ' + x)
+// }
+
+////////////////////////////////////////////////
+// OBJECTS AND ARRAYS
+////////////////////////////////////////////////
+// const names = ['Matthew', 'Seth']
+
+// const student = {
+//   name: 'Matthew',
+//   major: 'Computer Science',
+//   gradYear: 2023,
+// }
+
+// console.log(student.major)
+
+// const students = [
+//   {
+//     id: 1234,
+//     name: 'Matthew',
+//     major: 'Computer Science',
+//     age: 21,
+//     hasGraduated: false,
+//   },
+//   {
+//     id: 9876,
+//     name: 'Matthew',
+//     major: 'Computer Science',
+//     age: 22,
+//     hasGraduated: true,
+//   },
+// ]
+
+////////////////////////////////////////////////
+// FUNCTIONS
+////////////////////////////////////////////////
+// function multiply(x, y) {
+//   return x * y
+// }
+
+// const result = multiply(4, 6)
+// console.log(result)
+
+////////////////////////////////////////////////
+// FOR LOOPS
+////////////////////////////////////////////////
+
+// const names = ['Matthew', 'Seth', 'Bill', 'Weston', 'Ben']
+
+// for (let i = 0; i < names.length; i++) {
+//   console.log(names[i])
+// }
+
+// for (i in names) {
+//   console.log(names[i])
+// }
